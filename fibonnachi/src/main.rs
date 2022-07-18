8917eeec57f50f15d596d76aafb26e365b35f95e
Jens Peder Meldgaard <jens.peder.meldgaard@velux.com>
2022-07-18 16:38:48 +0200

    refactor inputs

--- fibonnachi/src/main.rs
+++ fibonnachi/src/main.rs
@@ -1,6 +1,19 @@
 use cached::proc_macro::cached;
 use std::io;
 
+fn get_input() -> i64 {
+    let mut input = String::new();
+
+    println!("Enter a number to get it's Fibonacci value");
+
+    io::stdin().read_line(&mut input).expect("Error");
+
+    return match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => -1,
+    };
+}
+
 #[cached]
 fn fibonacci(n: i64) -> i64 {
     if n <= 1 {
@@ -10,17 +23,7 @@ fn fibonacci(n: i64) -> i64 {
 }
 
 fn main() {
-    let mut input = String::new();
-
-    println!("Enter a number to get it's Fibonacci value");
-
-    io::stdin().read_line(&mut input).expect("Error");
-
-    let input = match input.trim().parse() {
-        Ok(num) => num,
-        Err(_) => -1,
-    };
-
+    let input = get_input();
     let result = fibonacci(input);
 
     println!("Fibonacci of {} is {}", input, result)

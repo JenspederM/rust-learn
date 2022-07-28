4a5ce441dca8b393df7fb9d10da3e51723ffa1e2
Jens Peder Meldgaard <jens.peder.meldgaard@velux.com>
2022-07-28 13:54:07 +0200

    Refactored utils

--- mqtt-client/src/utils.rs
+++ mqtt-client/src/utils.rs
@@ -4,10 +4,16 @@ use serde_json::Value;
 use std::env;
 
 pub fn env_default(key: &str, default: &str) -> String {
-    let masked = ["password", "cert"];
+    // Set partial keys where values should be masked in logs.
+    let mask_array = ["password".to_string(), "cert".to_string()];
+
+    // Mask any values where keys partially match those in `mask_array`.
+    let mask = mask_array.iter().any(|e| key.to_lowercase().contains(e));
+
+    // Get the value from the environment.
     match env::var(key) {
         Ok(s) => {
-            if masked.contains(&key.to_lowercase().as_str()) {
+            if mask {
                 log::info!("'{key}' set to '********'")
             } else {
                 log::info!("'{key}' set to '{s}'")
@@ -26,14 +32,17 @@ pub fn env_default(key: &str, default: &str) -> String {
 ///
 /// Here we pull also initialize defaults
 pub fn init_log() {
+    // Possibly load .env file
     dotenv().ok();
+    // Get the log level from the environment. Default to INFO.
     env_default("RUST_LOG", "info");
+    // Initiate logger.
     env_logger::init();
 }
 
 /// Turn a serde_json::Value into a string.
 ///
-/// This also removes "\"" since these are parsed literally by serde_json.
+/// Also removes "\"" since these are parsed literally by serde_json.
 pub fn value_to_string(v: &Value) -> String {
     // Values evaluate "" literally when parsing jons, hence we replace here.
     return v.to_string().trim().replace("\"", "");
